b28c92c8df162cd5c85b609e01bc1f1856201445
monse <78182277+dmmonse@users.noreply.github.com>
2022-05-03 10:49:29 -0500

    final functions
    added comments
    typo -9
    -20-23 clear display
    -25-27 clear -1 display

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+//links doc//
 let outputScreen = document.getElementById("output-screen");
 
+//links onclick with display//
 function display (num){
-    outputScreen .value += num;
+    outputScreen.value += num;
 
 }
+//to calculate = //
 function Calculate (){
     try{
-        outputScree.value= eval(outputScreen.value);
+        outputScreen.value= eval(outputScreen.value);
     }
+//error display //
     catch(err){
+        outputScreen.value= "";
         display("Invalid")
     }
+}
+//clear entire display //
+function Clear(){
+    outputScreen.value = "";
+}
+//clear -1 in display //
+function del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
 }
